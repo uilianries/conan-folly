1804dfc37ac45b0078f0c010fca216008bbe02f7
Uilian Ries <uilianries@gmail.com>
2018-12-05 16:24:31 -0200

    Link atomix
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -83,6 +83,7 @@ class FollyConan(ConanFile):
     def build(self):
         tools.patch(base_path=self._source_subfolder, patch_file='folly.patch')
         tools.replace_in_file(os.path.join(self._source_subfolder, "CMake", "FollyCompilerUnix.cmake"), "-std=${CXX_STD}", "-std=c++1z")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "target_link_libraries(folly PUBLIC folly_deps)", "target_link_libraries(folly PUBLIC folly_deps atomic)")
         cmake = self._configure_cmake()
         cmake.build()
 
